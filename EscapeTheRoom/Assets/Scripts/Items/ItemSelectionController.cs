64ad3ab826c07bdc4b217f59eabe6b4db747de1b
Dmytro <seaqqullprogramming@gmail.com>
2021-07-12 00:03:05 +0200

    Update: Remove unnecessary variable

--- EscapeTheRoom/Assets/Scripts/Items/ItemSelectionController.cs
+++ EscapeTheRoom/Assets/Scripts/Items/ItemSelectionController.cs
@@ -12,8 +12,6 @@ namespace EscapeTheRoom.Items
         private List<Renderer> _viewsRenderers;
         private List<Color> _colors;
 
-        private bool _selected;
-
 
         private void Awake()
         {
@@ -32,8 +30,6 @@ namespace EscapeTheRoom.Items
 
         public void OnSelect()
         {
-            _selected = true;
-
             for (int i = 0; i < _views.Count; i++)
             {
                 _viewsRenderers[i].material.color = _colors[i] * _colorScale;
@@ -42,8 +38,6 @@ namespace EscapeTheRoom.Items
 
         public void OnDeselect()
         {
-            _selected = false;
-
             for (int i = 0; i < _views.Count; i++)
             {
                 _viewsRenderers[i].material.color = _colors[i];
